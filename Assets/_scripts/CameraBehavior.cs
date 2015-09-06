132fb2ccdbc2d0f4e1757fcbdd87d21e2ade0b65
Matthew Popp <mgp214@gmail.com>
2015-09-06 00:47:13 -0500

    0.1.1 - revamped camera
    -adjusted input smoothing
    -ditched traditional RTS camera for alternate, mouselook camera.

--- Assets/_scripts/CameraBehavior.cs
+++ Assets/_scripts/CameraBehavior.cs
@@ -3,58 +3,78 @@ using System.Collections;
 
 public class CameraBehavior : MonoBehaviour {
 
-    public float cameraMoveSpeed, cameraRotateSpeed,cameraZoomSpeed, minHeight, maxHeight, minDegreesFromForward, maxDegreesFromForward,zoomFraction,zoomDistance;
+    public float cameraMoveSpeed,minHeight,maxHeight;
     private float zoomDistanceToLerp;
     private Vector3 zoomPreviousPosition;
 
-    private Transform pivotPointTransform;
+
+    Vector2 _mouseAbsolute;
+    Vector2 _smoothMouse;
+
+    public Vector2 clampInDegrees = new Vector2(360, 180);
+    public bool lockCursor;
+    public Vector2 sensitivity = new Vector2(2, 2);
+    public Vector2 smoothing = new Vector2(3, 3);
+    public Vector2 targetDirection;
+    public Vector2 targetCharacterDirection;
+
+
 
     void Start() {
-        pivotPointTransform = transform.GetChild(0);
-        zoomFraction = 0.5f;
-        zoomDistanceToLerp = 0f;
+        // Set target direction to the camera's initial orientation.
+        targetDirection = transform.localRotation.eulerAngles;
     }
+	
 	void Update () {
         MoveUpdate();
-        ZoomUpdate();
+        LookUpdate();
 	}
 
     private void MoveUpdate() {
-        //apply translation
-        float rightTranslation = cameraMoveSpeed * Input.GetAxis("Camera Strafe");
-        float forwardTranslation = cameraMoveSpeed * Input.GetAxis("Camera Forward");
-        Vector3 translationVector = new Vector3(rightTranslation,0f,forwardTranslation);
-        translationVector = transform.TransformDirection(translationVector);
+        //apply x,z translation
+        Vector3 translationVector = transform.TransformDirection(new Vector3(Input.GetAxis("Camera Strafe"), 0f, Input.GetAxis("Camera Forward")) * cameraMoveSpeed);
         transform.Translate(Vector3.ProjectOnPlane(translationVector,Vector3.up).normalized, Space.World);
-        //apply rotation
-        transform.RotateAround(pivotPointTransform.position, Vector3.up, cameraRotateSpeed/(pivotPointTransform.position-transform.position).magnitude*Input.GetAxis("Camera Rotation"));
+        //apply  y translation, clamped to min/max
+        transform.Translate(new Vector3(0f, Input.GetAxis("Camera Vertical") * cameraMoveSpeed),Space.World);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y,minHeight,maxHeight), transform.position.z);
     }
-
-    private void ZoomUpdate() {
-        float previousZoomFraction = zoomFraction;
-        zoomFraction -= Input.GetAxis("Camera Zoom") * cameraZoomSpeed;
-        zoomFraction = Mathf.Clamp01(zoomFraction);
-        float zoomDelta = zoomFraction - previousZoomFraction;
-        zoomDistanceToLerp -= zoomDelta * zoomDistance;
-        if ((transform.position - new Vector3(transform.position.x, Mathf.Lerp(minHeight, maxHeight, zoomFraction), transform.position.z)).magnitude > 0.0001f) {
-            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, Mathf.Lerp(minHeight, maxHeight, zoomFraction), transform.position.z), 0.1f);
-        }
-        if (Quaternion.Angle(transform.rotation, Quaternion.Slerp(Quaternion.Euler(minDegreesFromForward, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z),
-                                                                 Quaternion.Euler(maxDegreesFromForward, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z),
-                                                                 zoomFraction)) > 0.0001f) {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Slerp(Quaternion.Euler(minDegreesFromForward, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z),
-                                                  Quaternion.Euler(maxDegreesFromForward, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z),
-                                                  zoomFraction), 0.1f);
-        }
-        zoomPreviousPosition = transform.position;
-        if (Mathf.Abs(zoomDistanceToLerp) > 0.0001f) {
-            transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized * zoomDistanceToLerp, 0.1f);
-            if (zoomDistanceToLerp > 0) {
-                zoomDistanceToLerp -= (transform.position - zoomPreviousPosition).magnitude;
-            } else {
-                zoomDistanceToLerp += (transform.position - zoomPreviousPosition).magnitude;
-            }
-        }
-
-    }
+    private void LookUpdate()
+	{
+		// Ensure the cursor is always locked when set
+		if (lockCursor) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+		// Allow the script to clamp based on a desired target value.
+		var targetOrientation = Quaternion.Euler(targetDirection);
+		
+		// Get raw mouse input for a cleaner reading on more sensitive mice.
+		var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+		
+		// Scale input against the sensitivity setting and multiply that against the smoothing value.
+		mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity.x * smoothing.x, sensitivity.y * smoothing.y));
+		
+		// Interpolate mouse movement over time to apply smoothing delta.
+		_smoothMouse.x = Mathf.Lerp(_smoothMouse.x, mouseDelta.x, 1f / smoothing.x);
+		_smoothMouse.y = Mathf.Lerp(_smoothMouse.y, mouseDelta.y, 1f / smoothing.y);
+		
+		// Find the absolute mouse movement value from point zero.
+		_mouseAbsolute += _smoothMouse;
+		
+		// Clamp and apply the local x value first, so as not to be affected by world transforms.
+		if (clampInDegrees.x < 360)
+			_mouseAbsolute.x = Mathf.Clamp(_mouseAbsolute.x, -clampInDegrees.x * 0.5f, clampInDegrees.x * 0.5f);
+		
+		var xRotation = Quaternion.AngleAxis(-_mouseAbsolute.y, targetOrientation * Vector3.right);
+		transform.localRotation = xRotation;
+		
+		// Then clamp and apply the global y value.
+		if (clampInDegrees.y < 360)
+			_mouseAbsolute.y = Mathf.Clamp(_mouseAbsolute.y, -clampInDegrees.y * 0.5f, clampInDegrees.y * 0.5f);
+		
+		transform.localRotation *= targetOrientation;
+		
+		var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, transform.InverseTransformDirection(Vector3.up));
+		transform.localRotation *= yRotation;
+	}
 }
