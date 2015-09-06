3d5187af3dd149ea6bec84ada5c15f9f36229006
Matthew Popp <mgp214@gmail.com>
2015-09-05 23:46:56 -0500

    0.1.0 - Camera Control
    Implemented smooth, curved camera zooming (closes #4)
    milestone! woooo!

--- Assets/_scripts/CameraBehavior.cs
+++ Assets/_scripts/CameraBehavior.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class CameraBehavior : MonoBehaviour {
 
-    public float cameraMoveSpeed;
-    public float cameraRotateSpeed;
+    public float cameraMoveSpeed, cameraRotateSpeed,cameraZoomSpeed, minHeight, maxHeight, minDegreesFromForward, maxDegreesFromForward,zoomFraction,zoomDistance;
+    private float zoomDistanceToLerp;
+    private Vector3 zoomPreviousPosition;
 
     private Transform pivotPointTransform;
 
     void Start() {
-        pivotPointTransform = gameObject.transform.GetChild(0);
+        pivotPointTransform = transform.GetChild(0);
+        zoomFraction = 0.5f;
+        zoomDistanceToLerp = 0f;
     }
 	void Update () {
         MoveUpdate();
+        ZoomUpdate();
 	}
 
     private void MoveUpdate() {
@@ -20,9 +24,37 @@ public class CameraBehavior : MonoBehaviour {
         float rightTranslation = cameraMoveSpeed * Input.GetAxis("Camera Strafe");
         float forwardTranslation = cameraMoveSpeed * Input.GetAxis("Camera Forward");
         Vector3 translationVector = new Vector3(rightTranslation,0f,forwardTranslation);
-        translationVector = gameObject.transform.TransformDirection(translationVector);
-        gameObject.transform.Translate(Vector3.ProjectOnPlane(translationVector,Vector3.up), Space.World);
+        translationVector = transform.TransformDirection(translationVector);
+        transform.Translate(Vector3.ProjectOnPlane(translationVector,Vector3.up).normalized, Space.World);
         //apply rotation
-        gameObject.transform.RotateAround(pivotPointTransform.position, Vector3.up, cameraRotateSpeed/(pivotPointTransform.position-gameObject.transform.position).magnitude*Input.GetAxis("Camera Rotation"));
+        transform.RotateAround(pivotPointTransform.position, Vector3.up, cameraRotateSpeed/(pivotPointTransform.position-transform.position).magnitude*Input.GetAxis("Camera Rotation"));
+    }
+
+    private void ZoomUpdate() {
+        float previousZoomFraction = zoomFraction;
+        zoomFraction -= Input.GetAxis("Camera Zoom") * cameraZoomSpeed;
+        zoomFraction = Mathf.Clamp01(zoomFraction);
+        float zoomDelta = zoomFraction - previousZoomFraction;
+        zoomDistanceToLerp -= zoomDelta * zoomDistance;
+        if ((transform.position - new Vector3(transform.position.x, Mathf.Lerp(minHeight, maxHeight, zoomFraction), transform.position.z)).magnitude > 0.0001f) {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, Mathf.Lerp(minHeight, maxHeight, zoomFraction), transform.position.z), 0.1f);
+        }
+        if (Quaternion.Angle(transform.rotation, Quaternion.Slerp(Quaternion.Euler(minDegreesFromForward, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z),
+                                                                 Quaternion.Euler(maxDegreesFromForward, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z),
+                                                                 zoomFraction)) > 0.0001f) {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Slerp(Quaternion.Euler(minDegreesFromForward, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z),
+                                                  Quaternion.Euler(maxDegreesFromForward, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z),
+                                                  zoomFraction), 0.1f);
+        }
+        zoomPreviousPosition = transform.position;
+        if (Mathf.Abs(zoomDistanceToLerp) > 0.0001f) {
+            transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized * zoomDistanceToLerp, 0.1f);
+            if (zoomDistanceToLerp > 0) {
+                zoomDistanceToLerp -= (transform.position - zoomPreviousPosition).magnitude;
+            } else {
+                zoomDistanceToLerp += (transform.position - zoomPreviousPosition).magnitude;
+            }
+        }
+
     }
 }
--- Assets/_scripts/CameraPivotPointBehavior.cs
+++ Assets/_scripts/CameraPivotPointBehavior.cs
@@ -8,7 +8,7 @@ public class CameraPivotPointBehavior : MonoBehaviour {
     public LayerMask pivotLayerMask;
 	// Use this for initialization
 	void Start () {
-        parentTransform = gameObject.transform.parent;
+        parentTransform = transform.parent;
 	}
 	
 	// Update is called once per frame
@@ -16,12 +16,12 @@ public class CameraPivotPointBehavior : MonoBehaviour {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit rayHit;
         if (Physics.Raycast(ray, out rayHit, pivotPointDistance, pivotLayerMask)) {
-            gameObject.transform.position = rayHit.point;
+            transform.position = rayHit.point;
         } else {
             //fixed pivot distance
             Vector3 parentOffset = Vector3.ProjectOnPlane(parentTransform.forward, Vector3.up).normalized * pivotPointDistance;
-            gameObject.transform.position = parentTransform.position + parentOffset;
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, /*parentTransform.position.y*/0f, gameObject.transform.position.z);
+            transform.position = parentTransform.position + parentOffset;
+            transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
         }
 	}
 }
