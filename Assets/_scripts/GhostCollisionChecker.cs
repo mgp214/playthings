d24831cdbea80d92a295ee1438ca85b7e419d8e1
Matthew Popp <mgp214@gmail.com>
2015-09-08 20:15:47 -0500

    0.2.1 - fixed ghost transparency

--- Assets/_scripts/GhostCollisionChecker.cs
+++ Assets/_scripts/GhostCollisionChecker.cs
@@ -10,21 +10,21 @@ public class GhostCollisionChecker : MonoBehaviour {
     void OnTriggerEnter(Collider collider) {
         placementBlocked = true;
         foreach (Material mat in GetComponent<Renderer>().materials) {
-            mat.color = Color.red;
+            mat.color = new Color(1f, 0f, 0f, 0.5f);
         }
     }
     void OnTriggerStay(Collider collider) {
         if (!placementBlocked) {
             placementBlocked = true;
             foreach (Material mat in GetComponent<Renderer>().materials) {
-                mat.color = Color.red;
+                mat.color = new Color(1f,0f,0f,0.5f);
             }
         }
     }
     void OnTriggerExit(Collider colliders) {
         placementBlocked = false;
         foreach (Material mat in GetComponent<Renderer>().materials) {
-            mat.color = Color.white;
+            mat.color = new Color(1f, 1f, 1f, 0.5f);
         }
     }
 }
